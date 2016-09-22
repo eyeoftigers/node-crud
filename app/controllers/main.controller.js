ce275030d3dddc4f260e303841507684eca6b2ad
doctorede <waseemmachloy@gmail.com>
2016-09-22 14:04:08 +0300

    mm

---
+++ app/controllers/main.controller.js
@@ -0,0 +1,8 @@
+module.exports ={
+	//show home
+	showHome:(req,res)=>{
+		res.send('heeloo i am in controoler');
+		
+	}
+
+};
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,11 @@
 //creae new router
 
-const express = require('express') , router = express.Router();
+const express = require('express') ,
+	  router = express.Router() ,
+	  mainController = require('./controllers/main.controller');
 
 //export router
 module.exports = router;
+
+//define routes
+router.get('/',mainController.showHome);
